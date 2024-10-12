a4ee4e931d14d7651d7e12b9bf972c6052d88347
Alvian Naya Pradhana <32216035+alvinnaya@users.noreply.github.com>
2024-10-12 18:27:09 +0700

    Update db.js

--- db.js
+++ db.js
@@ -3,14 +3,6 @@ const { Pool,Client } = require('pg');
 const dotenv = require('dotenv');
 
 dotenv.config();
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'postgres',
-//   password: 'Rielia13',
-//   port: 5432,
-// });
 
-// const pool = new Client("postgresql://alvian:pX3gsnBameIpNViEPDbMBw@direct-tapir-7666.8nk.cockroachlabs.cloud:26257/project_f?sslmode=verify-full");
 const pool = new Client(process.env.DATABASE_URL);
 module.exports = pool;
